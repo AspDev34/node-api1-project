2ccfbe081b2094e9b3cebda7a51b039d180017c0
Ike Otu <ikeotu@msn.com>
2020-04-07 12:25:41 -0700

    refactored get request to return full array of users

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ const server = express();
 
 // endpoints
 server.get('/api/users', (req, res) => {
-    res.json({id: 1, name: 'Bruce Wayne', bio: 'I am only a philanthropist, nothing more!'},
+
+  const userArray = [{id: 1, name: 'Bruce Wayne', bio: 'I am only a philanthropist, nothing more!'},
     {id: 2, name: 'Charles Edward Cheese', bio: "Watch Dave Chappelle's standup for proper reference."},
-    {id: 3, name: 'Michael Scott', bio: "People say I am the world's best boss."})
+    {id: 3, name: 'Michael Scott', bio: "People say I am the world's best boss."}]
+
+    res.json(userArray);
 });
 
 const port = 5000; // the server is running on http://localhost:5000
