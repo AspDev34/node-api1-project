709954f9c256fee30051ea30038d30f518462772
Ike Otu <ikeotu@msn.com>
2020-04-07 13:25:39 -0700

    re-factored userArray to be global

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const express = require('express'); //similar to above method, but works in all 
 
 const server = express();
 
+
+const userArray = [{id: 1, name: 'Bruce Wayne', bio: 'I am only a philanthropist, nothing more!'},
+{id: 2, name: 'Charles Edward Cheese', bio: "Watch Dave Chappelle's standup for proper reference."},
+{id: 3, name: 'Michael Scott', bio: "People say I am the world's best boss."}];
+
 // endpoints
-server.get('/api/users', (req, res) => {
+server.get('/api/users', (req, res) => res.json(userArray));
 
-  const userArray = [{id: 1, name: 'Bruce Wayne', bio: 'I am only a philanthropist, nothing more!'},
-    {id: 2, name: 'Charles Edward Cheese', bio: "Watch Dave Chappelle's standup for proper reference."},
-    {id: 3, name: 'Michael Scott', bio: "People say I am the world's best boss."}]
-
-    res.json(userArray);
-});
+server.get('/api/users/:id', (req, res) => {
+    
+})
 
 const port = 5000; // the server is running on http://localhost:5000
 server.listen(port, () => console.log(`\n== api on port ${port} ==\n`));
