1035ba324a3978bb7397a1ed6cd1ada00ff50e4d
Ike Otu <ikeotu@msn.com>
2020-04-07 14:14:51 -0700

    get request by ID completed

--- index.js
+++ index.js
@@ -15,15 +15,28 @@ const userArray = [{id: 1, name: 'Bruce Wayne', bio: 'I am only a philanthropist
 // endpoints
 server.get('/api/users', (req, res) => res.json(userArray)); //returns full array of users
 
-// server.get('/api/users/:id', (req, res) => )
+// returns a user by their id
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
 
-server.post('/api/users', (req, res) => {
+    const userByID = userArray.find((userByID) => userByID.id == id);
+    
+    if (userByID) {
+        res.status(200).json(userByID);
+    }
+    else {
+        res.status(404).json({message: "That user id was not found"});
+    }
+});
+
+// Creates a user using the information sent inside the request body.
+server.post('/api/users', (req, res) => { 
     const userBody = req.body;
 
     userArray.push(userBody);
 
     res.status(201).json(userArray);
-}) // Creates a user using the information sent inside the request body.
+}) 
 
 const port = 5000; // the server is running on http://localhost:5000
 server.listen(port, () => console.log(`\n== api on port ${port} ==\n`));
